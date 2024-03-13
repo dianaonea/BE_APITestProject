969007f5f0e271ac3f7f183cbfe332e587e3edfc
Diana Cazacu <dcazacu@deloittece.com>
2024-03-13 15:18:37 +0200

    small change

--- src/main/java/com/myproject/accounts/AccountsManagementApplication.java
+++ src/main/java/com/myproject/accounts/AccountsManagementApplication.java
@@ -2,10 +2,8 @@ package com.myproject.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@SpringBootApplication (scanBasePackages = {"com.myproject.accounts.controller", "com.myproject.accounts.service"})
+@SpringBootApplication// (scanBasePackages = {"com.myproject.accounts.controller", "com.myproject.accounts.service"})
 public class AccountsManagementApplication {
 
     public static void main(String[] args) {
